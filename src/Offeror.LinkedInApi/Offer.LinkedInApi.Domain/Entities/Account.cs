8697e08fe9435c912adcfd08275afafed2dffe0a
TheWayToJunior <developermicrosoft1932@gmail.com>
2022-02-02 21:05:38 +0300

    Minor changes

--- src/Offeror.LinkedInApi/Offer.LinkedInApi.Domain/Entities/Account.cs
+++ src/Offeror.LinkedInApi/Offer.LinkedInApi.Domain/Entities/Account.cs
@@ -5,12 +5,19 @@ namespace Offeror.LinkedInApi.Domain.Entities
     public class Account : EntityBase
     {
         public string FirstName { get; set; }
+
         public string SecondName { get; set; }
+
         public string ProfileImage { get; set; }
+
         public string Url { get; set; }
+
         public Position Position { get; set; }
+
         public int PositionId { get; set; }
+
         public Region Region { get; set; }
+
         public int RegionId { get; set; }  
     }
 }
